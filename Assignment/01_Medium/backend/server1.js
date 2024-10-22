bfbb9c3d9d89d8e51f34282092ba15afbd6ff905
Pranav_Gupta08 <pranavgupta.kuna08@gmail.com>
2024-10-22 17:36:04 +0530

    update

--- Assignment/01_Medium/backend/server1.js
+++ Assignment/01_Medium/backend/server1.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 // Database Connection with MongoDB
-mongoose.connect("mongodb+srv://pranavguptakuna08:pranav08@cluster0.d673c.mongodb.net/reelbite", {
+mongoose.connect("", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
